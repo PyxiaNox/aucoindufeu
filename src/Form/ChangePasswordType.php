09f6afa3e43747fba06e7fa6e1a19b9aa1d67e82
PyxiaNox <kathleen.1d20@gmail.com>
2020-12-22 02:11:26 +0100

    symfony v0.1

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -10,66 +10,53 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\HttpKernel\Exception\LengthRequiredHttpException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use function Sodium\add;
 
-class RegisterType extends AbstractType
+class ChangePasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('firstname', TextType::class, [
-                'label' => 'Prénom',
-                'constraints' => new Length([
-                    'min' => 3,
-                    'max' => 50
-                ]),
-                'attr' => [
-                    'placeholder' => 'Renseignez votre prénom'
-                ]
+                'disabled' => true,
+                'label' => 'Mon prénom'
             ])
             ->add('lastname', TextType::class, [
-                'label' => 'Nom',
-                'constraints' => new Length([
-                    'min' => 3,
-                    'max' => 50
-                ]),
-                'attr' => [
-                    'placeholder' => 'Renseignez votre nom'
-                ]
+                'disabled' => true,
+                'label' => 'Mon nom'
             ])
             ->add('email', EmailType::class, [
-                'label' => 'Adresse mail',
-                'constraints' => new Length([
-                    'min' => 8,
-                    'max' => 150
-                ]),
+                'disabled' => true,
+                'label' => 'Mon adresse mail'
+            ])
+            ->add('old_password', PasswordType::class, [
+                'label' => 'Mon mot de passe actuel',
+                'mapped' => false,
                 'attr' => [
-                    'placeholder' => 'Renseignez votre adresse mail'
+                    'placeholder' => 'Veuillez saisir votre mot de passe actuel'
                 ]
             ])
-            ->add('password', RepeatedType::class, [
+            ->add('new_password', RepeatedType::class, [
                 'type' => PasswordType::class,
+                'mapped' => false,
                 'invalid_message' => 'Le mot de passe et sa confirmation doivent être identiques',
-                'label' => 'Mot de passe',
+                'label' => 'Mon nouveau mot de passe',
                 'required' => true,
                 'first_options' => [
-                    'label' => 'Mot de passe',
+                    'label' => 'Mon nouveau mot de passe',
                     'attr' => [
-                        'placeholder' => 'Renseignez votre mot de passe'
+                        'placeholder' => 'Renseignez votre nouveau mot de passe'
                     ]
                 ],
                 'second_options' => [
-                    'label' => 'Confirmation du mot de passe',
+                    'label' => 'Confirmation du nouveau mot de passe',
                     'attr' => [
-                        'placeholder' => 'Confirmez votre mot de passe'
+                        'placeholder' => 'Confirmez votre nouveau mot de passe'
                     ]
                 ],
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Inscription'
+                'label' => 'Mettre à jour'
             ])
         ;
     }
