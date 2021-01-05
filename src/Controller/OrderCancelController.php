5f465f7e0c4a185e1e62d6a595195f2c03847c0d
PyxiaNox <kathleen.1d20@gmail.com>
2021-01-05 02:30:08 +0100

    success and cancel orders

---
+++ src/Controller/OrderCancelController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderCancelController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/commande/erreur/{stripeSessionId}", name="order_cancel")
+     */
+    public function index($stripeSessionId): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneBy(['stripeSessionId' => $stripeSessionId]);
+
+        if (!$order || $order->getUser() != $this->getUser())
+        {
+            return $this->redirectToRoute('home');
+        }
+
+        // Envoyer un mail au client pour indiquer l'échec de paiement de la commande
+
+        return $this->render('order_cancel/index.html.twig', [
+            'order' => $order
+        ]);
+    }
+}
---
+++ src/Controller/OrderSuccessController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderSuccessController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/commande/merci/{stripeSessionId}", name="order_success")
+     */
+    public function index($stripeSessionId, Cart $cart): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneBy(['stripeSessionId' => $stripeSessionId]);
+
+        if (!$order || $order->getUser() != $this->getUser())
+        {
+            return $this->redirectToRoute('home');
+        }
+
+        $cart->remove();
+
+        // Modifier le status 'isPaid' de la commande en mettant "1"
+        if (!$order->getIsPaid())
+        {
+            $order->setIdPaid(1);
+            $this->entityManager->flush();
+        }
+
+        // Envoyer un mail au client pour confirmation de commande
+
+
+        return $this->render('order_success/index.html.twig', [
+            'order' => $order
+        ]);
+    }
+}
