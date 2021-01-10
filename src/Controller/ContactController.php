1c324b1849a3aec17d34b34f964984b6c1ee173f
PyxiaNox <kathleen.1d20@gmail.com>
2021-01-10 04:20:11 +0100

    contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/nous-contacter", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->addFlash('notice', 'Merci de nous avoir contacter, nous vous répondrons dans les meilleurs délais.');
+
+            $form->getData();
+
+            $mail = new Mail();
+            $mail->send('andre.claudel.bouquiniste@gmail.com', 'Au Coin du Feu', 'Demande de contact', 'Vous avez reçu une nouvelle demande de contact.');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
     }
 }
