b43dc5b80aa58c085b70a85137ea0f8f245df778
PyxiaNox <kathleen.1d20@gmail.com>
2021-01-15 23:46:44 +0100

    entity fix

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -49,7 +49,7 @@ class Product
     private $price;
 
     /**
-     * @ORM\ManyToOne(targetEntity=category::class, inversedBy="products")
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="products")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
